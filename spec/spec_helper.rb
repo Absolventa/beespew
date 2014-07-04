26bddb4b2001f910c7fd5fd1f968f8d1a24a7e8a
Carsten Zimmermann <carsten.zimmermann@absolventa.de>
2014-07-04 13:32:35 +0200

    Remove shoulda

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 require 'rspec/rails'
-require 'shoulda-matchers'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
